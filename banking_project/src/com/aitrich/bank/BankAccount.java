7bda3d5953c26d722d72d15412a7b760970869d9
AswinS <aswin3187@gmail.com>
2025-12-09 20:14:04 +0530

    added

--- banking_project/src/com/aitrich/bank/BankAccount.java
+++ banking_project/src/com/aitrich/bank/BankAccount.java
@@ -1,5 +1,128 @@
 package com.aitrich.bank;
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
 
-public class BankAccount {
+public class BankAccount implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    private String accountNumber;
+    private String accountType;     
+    private double balance;
+    private Customer customer;
+    private String status;          
+    private String hashedPin;       
+    private LocalDate createdDate;
+
+   
+
+    public BankAccount(String accountNumber, String accountType, double balance,
+                       Customer customer, String pin) {
+
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.customer = customer;
+        this.status = "ACTIVE";
+        this.createdDate = LocalDate.now();
+
+       
+        this.hashedPin = hashPin(pin);
+    }
+
+    
+
+    private String hashPin(String pin) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(pin.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing PIN", e);
+        }
+    }
+
+    
+
+    public boolean validatePin(String inputPin) {
+        String hashedInput = hashPin(inputPin);
+        return hashedInput.equals(this.hashedPin);
+    }
+
+   
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public String getHashedPin() {
+        return hashedPin;
+    }
+
+    public void setNewPin(String newPin) {
+        this.hashedPin = hashPin(newPin);
+    }
+
+   
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", accountType='" + accountType + '\'' +
+                ", balance=" + balance +
+                ", customer=" + (customer != null ? customer.getName() : "null") +
+                ", status='" + status + '\'' +
+                ", createdDate=" + createdDate +
+                '}';
+    }
 }
+
--- banking_project/src/com/aitrich/bank/Customer.java
+++ banking_project/src/com/aitrich/bank/Customer.java
@@ -1,6 +1,85 @@
 package com.aitrich.bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Customer {
+	private String customerId;
+	private String name;
+	private String email;
+	private String phoneNumber;
+	private String address;
+	private List<BankAccount>accounts=new ArrayList<>();
 	
+	
+	public Customer(String customerId, String name, String email, String phoneNumber, String address, List<BankAccount>accounts) {
+		this.customerId=customerId;
+		this.name=name;
+		this.email=email;
+		this.phoneNumber=phoneNumber;
+		this.address=address;
+		this.accounts=(accounts !=null)? accounts: new ArrayList<>();
+	}
+	
+	public void setCustomerId(String cId) {
+		customerId=cId;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	
+	
+	public void setName(String cName) {
+		name=cName;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setEmail(String mail) {
+		email=mail;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+	
+	public void setPhoneNumber(String phone) {
+		phoneNumber=phone;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setAddress(String addr) {
+		address=addr;
+	}
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAccounts(List<BankAccount>acc) {
+		accounts=acc;
+	}
+	public List<BankAccount>getAccounts(){
+		return accounts;
+	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+	    return "Customer{" +
+	           "customerId='" + customerId + '\'' +
+	           ", name='" + name + '\'' +
+	           ", email='" + email + '\'' +
+	           ", phoneNumber='" + phoneNumber + '\'' +
+	           ", address='" + address + '\'' +
+	           ", accounts=" + accounts +
+	           '}';
+	}
+
 
 }
